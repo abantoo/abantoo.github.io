e1c3f5946eeb6e4ef65771c43a8ffbf594666175
abantoo <abantonaimur@gmail.com>
2021-12-14 03:20:11 +0600

    added iframe and validation regex for email

--- scripts/table.js
+++ scripts/table.js
@@ -1,14 +1,15 @@
 "use strict";
-const feedback = document.querySelector(".feedback-btn");
 
 var i = 1;
 
-feedback.addEventListener("click", () => {
-  var tbl = document.getElementById("myTable");
-  var row = tbl.insertRow(i);
-  var cell1 = row.insertCell(0);
-  var cell2 = row.insertCell(1);
-  cell1.innerHTML = document.querySelector("#email").value;
-  cell2.innerHTML = document.querySelector("#message").value;
-  i++;
-});
+window.frames[2].document
+  .querySelector(".feedback-btn")
+  .addEventListener("click", () => {
+    var tbl = document.getElementById("myTable");
+    var row = tbl.insertRow(i);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    cell1.innerHTML = window.frames[2].document.querySelector("#email").value;
+    cell2.innerHTML = window.frames[2].document.querySelector("#message").value;
+    i++;
+  });
