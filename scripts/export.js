6ffc102c0a0ed9d767e17437606d81d5b4372b0b
abantoo <abantonaimur@gmail.com>
2021-12-15 16:52:29 +0600

    added options for export to either PDF or CSV, and fixed some issues with iframe

--- scripts/export.js
+++ scripts/export.js
@@ -1,59 +1,8 @@
 "use strict";
 
-// const exportIt = document
-//   .getElementById("iframe-feedbacks")
-//   .contentWindow.document.querySelector(".exportIt");
-
-const exportIt = document.querySelector(".exportIt");
-
-//related to csv download, need to explore this code
-
-function download_csv(csv, filename) {
-  var csvFile;
-  var downloadLink;
-
-  // CSV FILE
-  csvFile = new Blob([csv], { type: "text/csv" });
-
-  // Download link
-  downloadLink = document.createElement("a");
-
-  // File name
-  downloadLink.download = filename;
-
-  // We have to create a link to the file
-  downloadLink.href = window.URL.createObjectURL(csvFile);
-
-  // Make sure that the link is not displayed
-  downloadLink.style.display = "none";
-
-  // Add the link to your DOM
-  document.body.appendChild(downloadLink);
-
-  // Lanzamos
-  downloadLink.click();
-}
-
-function export_table_to_csv(html, filename) {
-  var csv = [];
-  var rows = document.querySelectorAll("table tr");
-
-  for (var i = 0; i < rows.length; i++) {
-    var row = [],
-      cols = rows[i].querySelectorAll("td, th");
-
-    for (var j = 0; j < cols.length; j++) row.push(cols[j].innerText);
-
-    csv.push(row.join(","));
-  }
-
-  // Download CSV
-  download_csv(csv.join("\n"), filename);
-}
-
-exportIt.addEventListener("click", () => {
-  var html = document.querySelector("table").outerHTML;
-  export_table_to_csv(html, "table.csv");
+document.querySelector("#exportAs").addEventListener("contextmenu", () => {
+  console.log("rightclick");
+  document.getElementById("exportAs").style.display = "none";
+  document.getElementById("export-options").style.display = "flex";
+  document.getElementById("export-options").style.gap = "20px";
 });
-
-//add message and email address to the table
--- scripts/table.js
+++ scripts/table.js
@@ -7,7 +7,7 @@ document.querySelector(".feedback-btn").addEventListener("click", () => {
   var row = tbl.insertRow(i);
   var cell1 = row.insertCell(0);
   var cell2 = row.insertCell(1);
-  cell1.innerHTML = window.frames[2].document.querySelector("#email").value;
-  cell2.innerHTML = window.frames[2].document.querySelector("#message").value;
+  cell1.innerHTML = document.querySelector("#email").value;
+  cell2.innerHTML = document.querySelector("#message").value;
   i++;
 });
