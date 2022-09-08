1116f11f2d526aa8d6fc64a76f3aa48dfe4b224f
abantoo <abantonaimur@gmail.com>
2022-09-09 00:25:16 +0600

    added some terminal functionalities

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,21 @@
-window.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('myid').addEventListener("click", () => {
-    console.log('clicked');
-  });
-});
+// window.addEventListener("DOMContentLoaded", function () {
+//   document.getElementById("myid").addEventListener("click", () => {
+//     console.log("clicked");
+//   });
+// });
+
+const executeCommand = (command) => {
+  if (command === "clear") {
+    return { data: "", status: true };
+  } else if (command === "--help") {
+    return {
+      data: "commands: --home --projects --about --feedback --clear",
+      status: true,
+    };
+  } else {
+    return {
+      data: "not a valid command --clear to clear the window",
+      status: false,
+    };
+  }
+};
